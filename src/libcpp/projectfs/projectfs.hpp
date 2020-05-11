a23576228ff39d405bc772832e4c78ed6395c00d
bosley <bosley117@gmail.com>
2020-05-11 16:17:21 -0400

    ProjectFS work

--- src/libcpp/projectfs/projectfs.hpp
+++ src/libcpp/projectfs/projectfs.hpp
@@ -1,3 +1,10 @@
+/*
+    Given a directory, the Project FS class will determine if the target is an application or 
+    library for NHLL. It will then load the project file structure in a manner that allows us to
+    easily obtain files and configurations relevant to compiling / interpreting 
+
+*/
+
 #ifndef NABLA_PROJECT_FS_HPP
 #define NABLA_PROJECT_FS_HPP
 
@@ -5,26 +12,22 @@ namespace NABLA
 {
     class ProjectFS
     {
-        public:
+    public:
 
-            enum class ProjectType
-            {
-                UNKNOWN,
-                LIBRARY,
-                PROJECT
-            };
+        enum class ProjectType
+        {
+            UNKNOWN,
+            LIBRARY,
+            APPLICATION
+        };
 
-            enum class LoadReturn
-            {
-                
-            };
+        ProjectFS();
+        ~ProjectFS();
 
-            ProjectFS();
-            ~ProjectFS();
+    private:
 
-
-
-        private:
+        bool projectLoaded;
+        ProjectType projectType;
     };
 }
 
