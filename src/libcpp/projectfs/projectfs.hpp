74d7692afdc22eff99734446c0197c170b301fdf
bosley <bosley117@gmail.com>
2020-05-10 21:42:29 -0400

    Started project lib dir work

---
+++ src/libcpp/projectfs/projectfs.hpp
@@ -0,0 +1,31 @@
+#ifndef NABLA_PROJECT_FS_HPP
+#define NABLA_PROJECT_FS_HPP
+
+namespace NABLA
+{
+    class ProjectFS
+    {
+        public:
+
+            enum class ProjectType
+            {
+                UNKNOWN,
+                LIBRARY,
+                PROJECT
+            };
+
+            enum class LoadReturn
+            {
+                
+            };
+
+            ProjectFS();
+            ~ProjectFS();
+
+
+
+        private:
+    };
+}
+
+#endif
