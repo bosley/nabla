2dd1f893d3ef3b64f7640dfdd540de6ae769fe82
bosley <bosley117@gmail.com>
2020-05-27 17:02:38 -0400

    Bison is dumb

--- install_compiler_deps.sh
+++ install_compiler_deps.sh
@@ -1,3 +1,13 @@
 sudo apt-get update
-sudo apt-get -y install flex=2.6.4-6.2
-sudo apt-get -y install bison=2:3.5.1+dfsg-1
+sudo apt-get -y install flex
+
+cd /tmp
+wget https://ftp.gnu.org/gnu/bison/bison-3.5.1.tar.gz
+tar -zxvf bison-3.5.1.tar.gz
+cd bison-3.5.1
+./configure
+sudo make
+sudo make install
+sudo ln -s /usr/local/bin/bison /usr/bin/bison
+
+# sudo apt-get -y install bison=2:3.5.1+dfsg-1
