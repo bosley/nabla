13250bac8e620b6f4441654b251f8f9de0b8fc38
bosley <bosley117@gmail.com>
2020-05-27 20:34:34 -0400

    Update install script

--- install_compiler_deps.sh
+++ install_compiler_deps.sh
@@ -1,6 +1,19 @@
-sudo apt-get update
-sudo apt-get -y install flex
+#
+#   Get the version of Flex that Nabla has been built with and install it
+#
+cd /tmp
+wget https://github.com/westes/flex/files/981163/flex-2.6.4.tar.gz
+tar -zxvf flex-2.6.4.tar.gz
+cd flex-2.6.4
+./configure
+sudo make 
+sudo make install 
+sudo rm /usr/bin/flex
+sudo ln -s /usr/local/bin/flex /usr/bin/flex
 
+#
+#   Get the version of Bison that Nabla has been build with and install it
+#
 cd /tmp
 wget https://ftp.gnu.org/gnu/bison/bison-3.5.1.tar.gz
 tar -zxvf bison-3.5.1.tar.gz
@@ -10,5 +23,3 @@ sudo make
 sudo make install
 sudo rm /usr/bin/bison
 sudo ln -s /usr/local/bin/bison /usr/bin/bison
-
-# sudo apt-get -y install bison=2:3.5.1+dfsg-1
